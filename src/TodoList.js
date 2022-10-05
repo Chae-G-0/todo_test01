4cba7c4daa05d036eb1902514951fc3fda39f899
chae <corkdud@naver.com>
2022-10-05 15:21:22 +0900

    221005

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const TodoList = () => {
+const TodoList = ({ list, setList }) => {
+    const handlerRemove = (id) => {
+        setList(list.filter((it) => id !== it.id));
+    };
     return (
-        <div>TodoList</div>
-    )
-}
+        <div>
+            <h2>list</h2>
+            <ul>
+                {list.map((li, idx) => (
+                    <li key={idx}>
+                        {li.title}
+                        {li.content}
+                        <button onClick={() => handlerRemove(li.id)}>DEL</button>
+                    </li>
+                ))}
+            </ul>
+            <hr />
+        </div>
+    );
+};
 
-export default TodoList
+export default TodoList;
--- src/TodoWrite.js
+++ src/TodoWrite.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const TodoWrite = () => {
+const TodoWrite = ({list, word, handlerWord, handlerList, inputTtitle, inputContent}) => {
     return (
-        <div>TodoWrite</div>
-    )
-}
+        <div>
+            <div>{console.log(list)}</div>
+            <div>
+                <input type="text" onChange={handlerWord} name="title" value={word.title || ""} ref={inputTtitle} />
+            </div>
+            <div>
+                <input type="text" onChange={handlerWord} name="content" value={word.content || ""} ref={inputContent} />
+            </div>
+            <div>
+                <button onClick={handlerList}>WRITE</button>
+            </div>
+        </div>
+    );
+};
 
-export default TodoWrite
+export default TodoWrite;
